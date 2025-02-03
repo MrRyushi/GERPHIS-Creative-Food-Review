ae38bdbb61af576073089b59eb6906479899bfd3
John Patrick Marcellana <jptmarcellana@gmail.com>
2025-02-03 22:50:12 +0800

    changed name, added init

---
+++ spicequest/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-900 h-screen w-screen flex justify-center items-center">
+      <div className="text-center space-y-4">
+        <h1 className="text-4xl text-slate-50">SpiceQuest</h1>
+        <p className="text-slate-50 text-2xl">Welcome to SpiceQuest!</p>
+      </div>
+    </div>
+  );
+}
