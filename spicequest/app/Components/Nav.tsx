3e6ae87661db9d206e7bff8d01b1ba81f3f877f0
Samantha Caasi <samcaasi30@gmail.com>
2025-02-09 23:21:34 +0800

    Fixed merge conflicts

--- spicequest/app/Components/Nav.tsx
+++ spicequest/app/Components/Nav.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Nav = () => {
   return (
     <div>{/* Nav Bar */}
-      <nav className="flex justify-between items-center bg-[#A49E7D] p-4 md:pe-20 lg:pe-28 md:ps-20 xl:pe-36 lg:ps-28 xl:ps-36 h-1/4 text-white w-screen">
+      <nav className="flex justify-between items-center bg-[#0C0C0C] p-4 md:pe-20 lg:pe-28 md:ps-20 xl:pe-36 lg:ps-28 xl:ps-36 h-1/4 text-white w-screen">
         <Link href="/" className="dm-serif-text text-xl">SpiceQuest</Link>
         <div className="space-x-12 figtree">
           <Link href="/countries" className='hover:underline underline-offset-4'>Home</Link>
--- spicequest/app/page.tsx
+++ spicequest/app/page.tsx
@@ -5,25 +5,27 @@ export default function Home() {
   return (
     <div 
       className="w-screen h-screen overflow-hidden bg-cover bg-center" 
-      style={{ backgroundImage: "url('/spicequest.png')" }} // Corrected path
+      style={{ backgroundImage: "url('/spicequest.png')" }}
     >
       {/* Header */}
       <div className="text-center space-y-4 font-light">
         <Nav />
       </div>
 
-      {/* Main */}
-<<<<<<< Updated upstream
+      {/* Main Content */}
       <div className="text-center flex justify-center items-center py-48">
         <div className="space-y-4 px-20 dm-serif-text text-white">
           <p className="text-2xl md:text-2xl lg:text-2xl xl:text-3xl">Welcome to </p>
           <p className="text-5xl md:text-5xl lg:text-6xl xl:text-7xl">SpiceQuest</p>
-          <p className="figtree text-lg md:text-lg lg:text-xl xl:text-2xl font-light pt-5">
+          <p className="figtree text-lg md:text-lg lg:text-xl xl:text-2xl font-light pt-5 px-10">
             Welcome to our Southeast Asian food review hub! 
             Explore delicious dishes from four SEA countries, carefully curated to create the perfect full-course meal. 
-            Get ready to indulge in the rich flavors and culinary wonders of Southeast Asia! 🍜🍛🥢</p>
-          <div>
-            <Link href="/countries" className="px-12 py-4 bg-nav hover:bg-lime-800 rounded-xl text-center lg:text-lg xl:text-xl">Continue</Link>
+            Get ready to indulge in the rich flavors and culinary wonders of Southeast Asia! 🍜🍛🥢
+          </p>
+          <div className="pt-8">
+            <Link href="/countries" className="px-12 py-4 hover:bg-[#DC6C3A] bg-[#D65D26] rounded-xl text-center lg:text-lg xl:text-xl">
+              Continue
+            </Link>
           </div>
         </div>
       </div>
