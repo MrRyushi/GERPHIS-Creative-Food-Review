811e8047daae07867e94abf85b52e37dc7c767e0
Samantha Caasi <samcaasi30@gmail.com>
2025-02-09 15:59:55 +0800

    added components for country rendering

--- spicequest/app/countries/page.tsx
+++ spicequest/app/countries/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link';
 import Nav from '../Components/Nav';
 import Image from 'next/image';
 
@@ -9,24 +10,28 @@ const CountryPage = () => {
     backgroundFood: "/indonesia_food.png",
     flag: "/indonesia.png",
     shortDesc: "Explore the rich flavors of Indonesian cuisine, from spicy rendang to sweet martabak.",
+    link: "/indonesia",
   },
   {
     name: "Philippines",
     backgroundFood: "/philippines_food.png",
     flag: "/philippines.png",
     shortDesc: "Discover the diverse flavors of Filipino cuisine, from savory adobo to sweet halo-halo.",
+    link: "/philippines",
   },
   {
     name: "Thailand",
     backgroundFood: "/thailand_food.png",
     flag: "/thailand.png",
     shortDesc: "Experience the vibrant flavors of Thai cuisine, from spicy tom yum to sweet mango sticky rice.",
+    link: "/thailand",
   },
   {
     name: "Vietnam",
     backgroundFood: "/vietnam_food.png",
     flag: "/vietnam.png",
     shortDesc: "Savor the fresh flavors of Vietnamese cuisine, from savory pho to sweet che.",
+    link: "/vietnam",
   }
 
 ]
@@ -45,10 +50,11 @@ const CountryPage = () => {
         {/* Main */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center items-center figtree md:px-20 lg:px-28 xl:px-36 gap-4 md:gap-4 lg:gap-4 xl:gap-8">
         {countriesData.map((country, index) => (        
-          <div key={index} className="group relative">
+          <Link key={index} href={country.link} className="group relative">
             <div className="relative grid h-[35rem] max-w-lg flex-col items-end justify-center overflow-hidden rounded-lg bg-white transition-transform duration-300 transform group-hover:scale-105">
               <div 
-                className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-cover bg-center"
+              // added after:absolute after:inset-0 after:bg-black/20 to make the white text stand out
+                className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-cover bg-center after:absolute after:inset-0 after:bg-black/25"
                 style={{ backgroundImage: `url(${country.backgroundFood})` }}
               >
                 <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50"></div>
@@ -69,7 +75,7 @@ const CountryPage = () => {
                 />
               </div>
             </div>
-          </div>
+          </Link>
         ))}
         </div>
       </div>
--- spicequest/app/page.tsx
+++ spicequest/app/page.tsx
@@ -14,7 +14,9 @@ export default function Home() {
       <div className="text-center flex justify-center items-center py-48 figtree">
         <div className="space-y-12 px-20">
           <p className="text-4xl md:text-4xl lg:text-5xl xl:text-6xl">WELCOME!</p>
-          <p className="text-lg md:text-lg lg:text-xl xl:text-2xl font-light ">Welcome to our Southeast Asian food review hub! Explore delicious dishes from four SEA countries, carefully curated to create the perfect full-course meal. Get ready to indulge in the rich flavors and culinary wonders of Southeast Asia! 🍜🍛🥢</p>
+          <p className="text-lg md:text-lg lg:text-xl xl:text-2xl font-light ">Welcome to our Southeast Asian food review hub! 
+            Explore delicious dishes from four SEA countries, carefully curated to create the perfect full-course meal. 
+            Get ready to indulge in the rich flavors and culinary wonders of Southeast Asia! 🍜🍛🥢</p>
           <div>
             <Link href="/countries" className="px-12 py-4 bg-nav hover:bg-lime-800 rounded-xl text-center lg:text-lg xl:text-xl">Continue</Link>
           </div>
